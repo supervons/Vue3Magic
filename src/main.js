0dd2d9970909416eb40e007f0090c7792e1c23b1
supervons <supervons@sina.com>
2023-02-15 22:45:25 +0800

    [upgrade] element-plus version to 2.2.30

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css';
 import { ElButton, ElSelect } from 'element-plus';
 
 const app = createApp(App)
