a9c94b1769cf8736cd8b7435ea774ca8996d44ac
supervons <supervons@sina.com>
2023-02-15 23:22:08 +0800

    [setup] Use setup change vue.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <el-button type="primary">主要按钮</el-button>
+  <el-button type="primary" @click="say">主要按钮{{ name }}</el-button>
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+let name = '流星'
+let age = 18
+//方法
+function say() {
+  console.log(`我叫${name},今年${age}岁`)
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,16 +4,9 @@
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
-}
+<script setup>
+const props = defineProps({
+  msg: String,
+})
+let count = 0
 </script>
