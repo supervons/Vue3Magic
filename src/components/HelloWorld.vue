aca7b3c140290fc3030799f24c7515c0dfa6c1da
supervons <supervons@sina.com>
2023-02-19 00:02:47 +0800

    Add watch and watchEffect demo.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup>
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive, computed, watch, watchEffect } from 'vue'
 
 const props = defineProps({
   msg: String,
@@ -19,6 +19,21 @@ let count = ref(0)
 
 let fullName = computed(() => {
   return `${hobby.join(' - ')} - ${count.value}`
+})
+
+// Functional watch
+watch(count, (newValue, oldValue) => {
+  console.log(`This count is add, current:${newValue} , old:${oldValue}`)
+})
+
+// Functional watch multiple value
+watch([count, () => [...hobby]], (newValue, oldValue) => { // watch object and array need like this.
+  console.log(`This count is add, current:${newValue} , old:${oldValue}`)
+})
+
+watchEffect(() => {
+  let tempCount = count
+  console.log('exec:' + tempCount.value)
 })
 
 function add() {
